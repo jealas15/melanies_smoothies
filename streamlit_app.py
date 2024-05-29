df66c60147c8b53328072cd7cb275a30afdda48d
jealas15 <joseph.ernest.c.alas@accenture.com>
2024-05-29 14:31:52 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,7 @@ st.write("The name on your Smoothie will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
@@ -43,6 +44,20 @@ ingredients_list = st.multiselect(
     ,my_dataframe
     ,max_selections = 5
     )
+
+
+import requests
+
+if ingredients_list:
+	ingredients_string = ''
+
+	for fruit_chosen in ingredients_list: 
+        ingredients_string += fruit_chosen + ' '  
+	st.subheader(fruit_chosen + ' Nutrition Information')
+	fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+	fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
+
 
 if ingredients_list:
     ingredients_string = ''
@@ -65,13 +80,6 @@ if ingredients_list:
 	    
         
         st.success('Your Smoothie is ordered!', icon="✅")
-
-# New section to display fruityvice nutrition information
-
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 
 
