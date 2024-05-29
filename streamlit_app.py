f16e28ff6d13ff18194e0d691cc964bd77b99d71
jealas15 <joseph.ernest.c.alas@accenture.com>
2024-05-29 14:33:22 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,17 @@ st.write("The name on your Smoothie will be:", name_on_order)
 
 #st.write("You favorite fruit is:", option)
 
+import requests
+
+if ingredients_list:
+	ingredients_string = ''
+
+	for fruit_chosen in ingredients_list: 
+	        ingredients_string += fruit_chosen + ' '  
+		st.subheader(fruit_chosen + ' Nutrition Information')
+		fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+		fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
 
 
 
@@ -45,17 +56,6 @@ ingredients_list = st.multiselect(
     ,max_selections = 5
     )
 
-
-import requests
-
-if ingredients_list:
-	ingredients_string = ''
-
-	for fruit_chosen in ingredients_list: 
-	        ingredients_string += fruit_chosen + ' '  
-		st.subheader(fruit_chosen + ' Nutrition Information')
-		fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
-		fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 
 
